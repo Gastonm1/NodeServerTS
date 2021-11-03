b6c2b8876987ee50e6609a780b666ce8a97513c2
Gastonm1 <gaston.marques@gmail.com>
2021-11-03 16:03:53 -0500

    Setup webserver to listen on local 3k

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("Hello, World")
+//From built in http module from node
+import http, { IncomingMessage, ServerResponse } from 'http';
+import path from "path";
+import fs from "fs/promises"
+
+async function requestListener(req: IncomingMessage, res:ServerResponse){
+    const filePath = path.join(__dirname, 'static/index.html')
+    const data = await fs.readFile(filePath, "utf-8")
+
+    res.writeHead(200, {
+        "Content-type" : "text/html",
+        "content-length": data.length
+    })
+    res.write(data);
+    res.end();
+
+
+}
+
+http.createServer(requestListener).listen(3000, () => {
+    console.log("HTTP server listening on port 3000")
+})
