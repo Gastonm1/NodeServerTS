4918afcdf74f20a1cee3c29f11d3939021b1e64b
Gastonm1 <gaston.marques@gmail.com>
2021-11-03 16:22:52 -0500

    Added custom routes

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,30 @@
 //From built in http module from node
 import http, { IncomingMessage, ServerResponse } from 'http';
 import path from "path";
+//read files w/o callbacks
 import fs from "fs/promises"
+import url from 'url';
 
+//request listener w/ types
 async function requestListener(req: IncomingMessage, res:ServerResponse){
-    const filePath = path.join(__dirname, 'static/index.html')
-    const data = await fs.readFile(filePath, "utf-8")
+    const parsedURL = url.parse(req.url || "");
 
-    res.writeHead(200, {
-        "Content-type" : "text/html",
+    let data:string = ""
+    let statusCode:number = 200
+    try{
+        let pathName = parsedURL.pathname;
+        if(pathName === "/"){pathName = "/index";} 
+        const filePath = path.join(__dirname, `static${pathName}.html}`)
+        data = await fs.readFile(filePath, "utf-8")
+    } catch {
+        data = await fs.readFile(path.join(__dirname, "static/404.html"), "utf-8")
+        statusCode= 404
+    }
+
+    
+
+    res.writeHead(statusCode, {
+        "Content-type": "text/html",
         "content-length": data.length
     })
     res.write(data);
