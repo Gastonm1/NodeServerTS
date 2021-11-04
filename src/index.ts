e62fa177d745dd35a2836e5a10ac79f2db5a894c
Gastonm1 <gaston.marques@gmail.com>
2021-11-03 19:05:13 -0500

    Routes

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,48 @@
-//From built in http module from node
-import http, { IncomingMessage, ServerResponse } from 'http';
+import http, { IncomingMessage, ServerResponse } from "http";
 import path from "path";
-//read files w/o callbacks
-import fs from "fs/promises"
-import url from 'url';
+import fs from "fs/promises";
+import url from "url";
+import fetch from "node-fetch";
 
-//request listener w/ types
-async function requestListener(req: IncomingMessage, res:ServerResponse){
-    const parsedURL = url.parse(req.url || "");
+interface Joke {
+  id: string;
+  joke: string;
+}
 
-    let data:string = ""
-    let statusCode:number = 200
-    try{
-        let pathName = parsedURL.pathname;
-        if(pathName === "/"){pathName = "/index";} 
-        const filePath = path.join(__dirname, `static${pathName}.html}`)
-        data = await fs.readFile(filePath, "utf-8")
-    } catch {
-        data = await fs.readFile(path.join(__dirname, "static/404.html"), "utf-8")
-        statusCode= 404
-    }
+async function requestListener(req: IncomingMessage, res: ServerResponse) {
+  const parsedUrl = url.parse(req.url || "");
 
-    
+  let data = "";
+  try {
+    let pathName = parsedUrl.pathname;
+    if (pathName === "/") pathName = "/index";
+    const filePath = path.join(__dirname, `static${pathName}.html`);
+    data = await fs.readFile(filePath, "utf-8");
+  } catch {
+    data = await fs.readFile(path.join(__dirname, "static/404.html"), "utf-8");
+  }
 
-    res.writeHead(statusCode, {
-        "Content-type": "text/html",
-        "content-length": data.length
-    })
-    res.write(data);
-    res.end();
+//   // Special handling of the dad joke path.
+//   if (parsedUrl.pathname === "/dad-joke") {
+//     const response = await fetch("https://icanhazdadjoke.com", {
+//       headers: {
+//         accept: "application/json",
+//         "user-agent": "NodeJS Server",
+//       },
+//     });
+//     const joke = await response.json();
 
+//     data = data.replace(/{{joke}}/gm, JSON.stringify(joke));
+//   }
 
+  res.writeHead(200, {
+    "Content-Type": "text/html",
+    "Content-Length": data.length,
+  });
+  res.write(data);
+  res.end();
 }
 
 http.createServer(requestListener).listen(3000, () => {
-    console.log("HTTP server listening on port 3000")
-})
+  console.log("HTTP Server listening on port 3000");
+});
